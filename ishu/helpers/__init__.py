6fa6f5afcdcff10e6a334b6d4d8f1d44b233a4c7
ᯓ꯭𓆰꯭×꯭🔥⁠⁠⁠𝐕꯭𝐀꯭𝐌꯭𝐏꯭𝐈꯭𝐑꯭𝐄꯭ 𝐊꯭𝐈꯭𝐍꯭𝐆 ꯭🌾꯭ <mdkhateeb07302@gmail.com>
2026-07-10 21:00:01 +0530

    Update __init__.py

--- ishu/helpers/__init__.py
+++ ishu/helpers/__init__.py
@@ -4,7 +4,7 @@
 
 from ._admins import admin_check, can_manage_vc, is_admin, reload_admins
 from ._dataclass import Media, Track
-from ._exec import format_exception, nval
+from ._exec import format_exception
 from ._inline import Inline
 from ._queue import Queue
 from ._thumbnails import Thumbnail
