79dacb31b521831516f2164944e3be2582701e57
ᯓ꯭𓆰꯭×꯭🔥⁠⁠⁠𝐕꯭𝐀꯭𝐌꯭𝐏꯭𝐈꯭𝐑꯭𝐄꯭ 𝐊꯭𝐈꯭𝐍꯭𝐆 ꯭🌾꯭ <mdkhateeb07302@gmail.com>
2026-07-10 23:29:23 +0530

    Update __init__.py

--- ishu/helpers/__init__.py
+++ ishu/helpers/__init__.py
@@ -3,7 +3,7 @@
 # This file is part of AnonXMusic
 
 
-from ._admins import admin_check, can_manage_vc, cmd_delete, is_admin, reload_admins
+from ._admins import admin_check, can_manage_vc, is_admin, reload_admins
 from ._dataclass import Media, Track
 from ._exec import format_exception, meval
 from ._inline import Inline
