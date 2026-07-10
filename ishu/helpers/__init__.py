fb3e458fffa3b3d996744fc45b4c28128a9d5b9c
ᯓ꯭𓆰꯭×꯭🔥⁠⁠⁠𝐕꯭𝐀꯭𝐌꯭𝐏꯭𝐈꯭𝐑꯭𝐄꯭ 𝐊꯭𝐈꯭𝐍꯭𝐆 ꯭🌾꯭ <mdkhateeb07302@gmail.com>
2026-07-10 20:54:09 +0530

    Update __init__.py

--- ishu/helpers/__init__.py
+++ ishu/helpers/__init__.py
@@ -1,11 +1,10 @@
-# Copyright (c) 2025 AnonymousX1025
+# Copyright (C) 2025 AnonymousX1025
 # Licensed under the MIT License.
 # This file is part of AnonXMusic
 
-
 from ._admins import admin_check, can_manage_vc, is_admin, reload_admins
 from ._dataclass import Media, Track
-from ._exec import format_exception, meval
+from ._exec import format_exception, nval
 from ._inline import Inline
 from ._queue import Queue
 from ._thumbnails import Thumbnail
