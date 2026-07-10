89304b15e3d8c7da299d54e5aa071826c0d8a852
ᯓ꯭𓆰꯭×꯭🔥⁠⁠⁠𝐕꯭𝐀꯭𝐌꯭𝐏꯭𝐈꯭𝐑꯭𝐄꯭ 𝐊꯭𝐈꯭𝐍꯭𝐆 ꯭🌾꯭ <mdkhateeb07302@gmail.com>
2026-07-10 18:16:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,8 +26,8 @@ class Config:
         self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/llDARK_WORLDll")
 
         # Railway self-hosted YouTube API
-        self.RAILWAY_YT_API_URL = getenv("RAILWAY_YT_API_URL", "")
-        self.RAILWAY_YT_API_KEY = getenv("RAILWAY_YT_API_KEY", "")
+        self.RAILWAY_YT_API_URL = getenv("RAILWAY_YT_API_URL", "youtube-api-music-production-77fb.up.railway.app")
+        self.RAILWAY_YT_API_KEY = getenv("RAILWAY_YT_API_KEY", "jd7hSNs6ilyVu4i~EkFx4_yONJhz.5bx")
 
 
         self.DEFAULT_THUMB = getenv("DEFAULT_THUMB", "https://te.legra.ph/file/3e40a408286d4eda24191.jpg")
